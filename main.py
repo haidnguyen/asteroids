da86fcf7718f2a3dda03d513f5ffed0fdcf5e956
Nguyen Dinh Hai <haind@anymindgroup.com>
2024-09-15 02:07:03 +0700

    finish the game

--- main.py
+++ main.py
@@ -1,16 +1,58 @@
 import pygame
+import sys
+
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+    dt = 0
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill((0, 0, 0))
+        
+        for item in updatable:
+            item.update(dt)
+        
+        for item in asteroids:
+            if item.is_collision(player):
+                print("Game over!")
+                sys.exit()
+            for bullet in shots:
+                if item.is_collision(bullet):
+                    item.split()
+                    bullet.kill()
+
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
+        
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
